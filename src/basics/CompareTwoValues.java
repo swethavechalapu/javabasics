4cdc1e2574bcbcc34b782be3e4b91ee55dc0eb84
Swetha V <swethavechalapu49@gmail.com>
2023-02-03 15:11:06 +0530

    which one is bigger in two values

---
+++ src/basics/CompareTwoValues.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class CompareTwoValues {
+	
+	
+	public static void main(String[] args) {
+	int a,b,c ;
+	a=10;
+	b=20;
+	if(a>b) {
+		System.out.println("a is bigger than b");
+	}
+	if(a<b) {
+		System.out.println("b is bigger than a");
+	}
+	
+}
+}
