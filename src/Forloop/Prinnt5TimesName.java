6267cc5577e796db10481c9be4d1b36d3592495f
Swetha V <swethavechalapu49@gmail.com>
2023-02-08 12:31:21 +0530

    ForLoopPractice

---
+++ src/Forloop/Prinnt5TimesName.java
@@ -0,0 +1,12 @@
+package Forloop;
+
+public class Prinnt5TimesName {
+	public static void main(String[] args) {
+		int n=10;
+		for(int i=0;i<=n;i++) {
+		  System.out.println("swetha");
+			
+		}
+	}
+
+}
---
+++ src/basics/forloop.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class forloop {
+	public static void main(String[] args) {
+	
+		int i=1;
+		for(i=1;i<=10;i++) {
+			
+			if (i!=5) {
+				System.out.println(i);
+			} else {
+				System.out.println("This is my lucky number");
+			}
+		
+	    }
+		
+		
+//	for(i=2;i<5;i++) {
+//		if(i==2) {
+//			System.out.println("My Name is One");
+//			}
+//	}
+		
+		
+	}
+}
+
